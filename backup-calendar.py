1241bec356f7097f34296136baf8cf6648caba0b
Jiajie Chen <c@jia.je>
2025-03-10 22:40:45 +0800

    Add calendar backup functionality

---
+++ backup-calendar.py
@@ -0,0 +1,143 @@
+from threading import Thread
+from typing import List
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import sys
+import json
+import os
+import argparse
+
+from urllib.parse import quote
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from secret import *
+
+# docs: https://open.feishu.cn/document/server-docs/calendar-v4/overview
+
+app_access_token = ""
+tenant_access_token = ""
+user_access_token = ""
+filter = None
+
+
+def init():
+    # get app_access_token and tenant_access_token
+    resp = requests.post(
+        "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal",
+        json={"app_id": app_id, "app_secret": app_secret},
+    ).json()
+
+    global app_access_token
+    app_access_token = resp["app_access_token"]
+
+    global tenant_access_token
+    tenant_access_token = resp["tenant_access_token"]
+
+    print("Tenant Access Token:", tenant_access_token)
+    print("App Access Token:", app_access_token)
+
+
+# utility
+
+
+def get(url, access_token):
+    # retry logic from https://gist.github.com/benjiao/28dc36bd87121b3273e0b3e079a8e8d8
+    retries = Retry()
+    with requests.Session() as s:
+        s.mount("http://", HTTPAdapter(max_retries=retries))
+        s.mount("https://", HTTPAdapter(max_retries=retries))
+
+        resp = s.get(url, headers={"Authorization": f"Bearer {access_token}"})
+        json = resp.json()
+        if json["code"] != 0:
+            print(f"Request to {url} failed with: {json}")
+            sys.exit(1)
+        return json["data"]
+
+
+state = "backup"
+redirect_uri = quote("http://127.0.0.1:8888/backup")
+url = f"https://open.feishu.cn/open-apis/authen/v1/index?redirect_uri={redirect_uri}&app_id={app_id}&state={state}"
+print(f"Please open {url} in browser")
+
+
+def work(code):
+    resp = requests.post(
+        "https://open.feishu.cn/open-apis/authen/v1/access_token",
+        headers={"Authorization": f"Bearer {app_access_token}"},
+        json={"grant_type": "authorization_code", "code": code},
+    ).json()
+
+    global user_access_token
+    user_access_token = resp["data"]["access_token"]
+
+    # list calendars
+    calendars = get(
+        "https://open.feishu.cn/open-apis/calendar/v4/calendars?page_size=500",
+        user_access_token,
+    )
+    calendars = calendars["calendar_list"]
+    print(f"Found {len(calendars)} calendars")
+
+    for calendar in calendars:
+        calendar_id = calendar["calendar_id"]
+        print(f"Handling calendar {calendar['summary']} {calendar_id}")
+
+        page_token = None
+        while True:
+            if page_token is not None:
+                events_url = f"https://open.feishu.cn/open-apis/calendar/v4/calendars/{calendar_id}/events?page_token={page_token}&anchor_time=0"
+            else:
+                events_url = f"https://open.feishu.cn/open-apis/calendar/v4/calendars/{calendar_id}/events?anchor_time=0"
+
+            data = get(
+                events_url,
+                user_access_token,
+            )
+            events = data["items"]
+            print(f"Found {len(events)} events")
+
+            for event in events:
+                event_id = event["event_id"]
+                with open(
+                    f"{backup_path}/calendar/{event_id}.json", "w", encoding="utf-8"
+                ) as file:
+                    print(event, file=file)
+
+            if data["has_more"]:
+                page_token = data["page_token"]
+            else:
+                break
+    print("Finished!")
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("Done!".encode("utf-8"))
+
+        code = parse_qs(urlparse(self.path).query).get("code", None)
+        if code is None:
+            return
+
+        code = code[0]
+        thread = Thread(target=work, args=(code,))
+        thread.start()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Backup feishu calendar events")
+
+    args = parser.parse_args()
+
+    init()
+    server_address = ("", 8888)
+    httpd = HTTPServer(server_address, Server)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
